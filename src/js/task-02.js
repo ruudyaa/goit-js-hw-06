6b56e26bd8e667c1330dd5bcbc8ca9b4f723e5b5
ruudyaa <martarudnytska26@gmail.com>
2023-03-18 19:46:14 +0200

    Adds task 2.

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
+"use strict";
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  listEl.appendChild(liEl);
+}
+
+console.log(listEl);
