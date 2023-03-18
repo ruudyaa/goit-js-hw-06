2e325cff0aacb5fc947ed7174e58c3a1e22ee5f4
ruudyaa <martarudnytska26@gmail.com>
2023-03-18 22:58:05 +0200

    Adds task 5.

---
+++ src/js/task-05.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const refs = {
+  input: document.querySelector("#name-input"),
+  span: document.querySelector("#name-output"),
+};
+
+refs.input.addEventListener("input", onInputFocus);
+
+function onInputFocus(event) {
+  if (refs.input.value === "") {
+    refs.span.textContent = "Anonymous";
+  } else {
+    refs.span.textContent = event.currentTarget.value;
+  }
+}
