8ed9e11b22c92ff629148c7e19a05455ed3fbc0f
ruudyaa <martarudnytska26@gmail.com>
2023-03-19 01:03:31 +0200

    Adds task 8.

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+"use strict";
 
+const form = document.querySelector(".login-form");
+const formData = {};
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  if (formElements.email.value === "" || formElements.password.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+    formData.email = formElements.email.value;
+    formData.password = formElements.password.value;
+
+    console.log(formData);
+  }
+  event.currentTarget.reset();
+}
