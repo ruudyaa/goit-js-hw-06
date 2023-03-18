978634c7e90e9cff6be172e139337d0395e806ec
ruudyaa <martarudnytska26@gmail.com>
2023-03-18 23:53:18 +0200

    Adds task 7.

---
+++ src/js/task-07.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const fontControler = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+fontControler.addEventListener("input", onInputFontChange);
+
+function onInputFontChange() {
+  text.style.fontSize = fontControler.value + "px";
+}
