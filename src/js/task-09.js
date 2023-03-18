efda98445fc53b2c39eb3cd9eebb99b1193918a6
ruudyaa <martarudnytska26@gmail.com>
2023-03-19 01:15:49 +0200

    Adds task 9.

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,17 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const body = document.body;
+const textColor = document.querySelector(".color");
+const btnColorChanger = document.querySelector(".change-color");
+
+btnColorChanger.addEventListener("click", ChangeColor);
+
+function ChangeColor() {
+  textColor.textContent = body.style.backgroundColor = getRandomHexColor();
 }
