8d8cc13eeb1cace52d9f0b37ba29ab21a7aadf83
ruudyaa <martarudnytska26@gmail.com>
2023-03-18 21:18:14 +0200

    Adds task 4.

---
+++ src/js/task-04.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+const value = document.querySelector("#value");
+let counterValue = 0;
+
+btnDecrement.addEventListener("click", decrementValue);
+btnIncrement.addEventListener("click", incrementValue);
+
+function decrementValue() {
+  counterValue -= 1;
+  value.textContent = counterValue;
+}
+
+function incrementValue() {
+  counterValue += 1;
+  value.textContent = counterValue;
+}
