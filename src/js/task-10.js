5f74ca4979f14de005e2359d3479bcf8c47da4a4
ruudyaa <martarudnytska26@gmail.com>
2023-03-19 20:03:14 +0200

    Corrects task 10.

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -10,28 +10,17 @@ const btnCreateEl = document.querySelector("button[data-create]");
 const btnDestroyEl = document.querySelector("button[data-destroy]");
 const boxesEl = document.querySelector("#boxes");
 
-inputEl.addEventListener("input", toogleValue);
 btnCreateEl.addEventListener("click", () => {
   createBoxes(Number(inputEl.value));
 });
 btnDestroyEl.addEventListener("click", destroyBoxes);
 
-let inputValue = 0;
-function toogleValue(event) {
-  inputValue = event.currentTarget.value;
-  console.log(inputValue);
-}
-
 function createBoxes(amount) {
   let divArray = [];
-  let sizeDefaultValue = 30;
   for (let i = 0; i < amount; i += 1) {
-    sizeDefaultValue += 10;
     const divElement = document.createElement("div");
-    divElement.classList = "item";
-    divElement.style.height = `${sizeDefaultValue}px`;
-    divElement.style.width = `${sizeDefaultValue}px`;
-    divElement.style.marginRight = "30px";
+    divElement.style.height = 30 + 10 * i + "px";
+    divElement.style.width = 30 + 10 * i + "px";
     divElement.style.marginBottom = "30px";
     divElement.style.backgroundColor = getRandomHexColor();
     divArray.push(divElement);
@@ -42,5 +31,5 @@ function createBoxes(amount) {
 
 function destroyBoxes() {
   inputEl.value = "";
-  return (boxesEl.innerHTML = "");
+  boxesEl.innerHTML = "";
 }
